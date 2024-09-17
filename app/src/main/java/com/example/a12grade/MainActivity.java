c4e7f1081b90b976dd948419a2a778ca707789c4
zidan <zidanmuhammed2012@gmail.com>
2024-09-17 09:17:20 +0300

    mhmd

--- app/src/main/java/com/example/a12grade/MainActivity.java
+++ app/src/main/java/com/example/a12grade/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
-Button b;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
